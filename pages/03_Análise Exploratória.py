a75791e80967619817fa31077def1b4303fd6b67
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2023-10-28 18:53:17 -0300

    Update 03_Análise Exploratória.py

--- pages/03_Análise Exploratória.py
+++ pages/03_Análise Exploratória.py
@@ -15,21 +15,21 @@ with tab1:
 
         
         st.markdown("""
-        ## Análise Exploratória: Uma Jornada pelos Impactos da Pandemia
+        # Análise Exploratória: Uma Jornada pelos Impactos da Pandemia
 
         Durante três intensos meses, o mundo enfrentou mais do que uma crise de saúde. A COVID-19 revelou-se uma tempestade perfeita, afetando todos os aspectos de nossa sociedade: desde a saúde até a economia, passando pelas relações sociais e padrões de comportamento.
 
         Neste cenário, cada dado, cada número, conta uma história. E é nossa missão, através desta análise exploratória, dar voz a essas histórias silenciosas, mas profundamente reveladoras. Vamos nos aprofundar nas camadas da sociedade, entender os desafios enfrentados por diferentes grupos e descobrir as nuances ocultas por trás dos números brutos.
 
-        ### Nossos Temas de Exploração:
+        ## Nossos Temas de Exploração:
 
-        #### Sociodemográfico:
+        ### Sociodemográfico:
         Aqui, exploraremos como diferentes segmentos da população foram afetados. Como a pandemia influenciou diferentes faixas etárias, gêneros e grupos socioeconômicos? Quais foram os padrões de comportamento adotados e como eles se refletiram na propagação do vírus?
 
-        #### Clínico:
+        ### Clínico:
         A saúde é, obviamente, o epicentro desta crise. Mas quais sintomas foram mais comuns? Existem correlações entre certos sintomas e a gravidade da doença? Como o acesso a serviços de saúde influenciou os resultados para os pacientes?
 
-        #### Econômico:
+        ### Econômico:
         A economia foi profundamente afetada pela pandemia. Mas como exatamente? Quais grupos foram mais vulneráveis? Como os padrões de trabalho mudaram e quais foram as implicações econômicas de longo prazo?
 
         
