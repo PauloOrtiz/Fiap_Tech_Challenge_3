d61f30f653ffa445d296c64449a3adb703253d19
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2023-10-28 19:00:50 -0300

    Update 05_K-means.py

--- pages/05_K-means.py
+++ pages/05_K-means.py
@@ -14,7 +14,32 @@ tab1, tab2, tab3, tab4, tab5 = st.tabs(["Introdução","Análise de Correlação
 
 
 with tab1:
-        pass
+        st.markdown("""
+        # K-means: A Arte de Segmentar
+
+        Imagine que você está em um grande salão de festas, com pessoas de todas as idades, profissões e interesses. Seu desafio é organizar pequenos grupos de conversa, onde cada pessoa tem algo em comum com os outros membros do seu grupo. Como você faria isso? O algoritmo K-means pode ser sua solução!
+
+        ## Como funciona o K-means?
+
+        O K-means é como um maestro habilidoso que organiza uma orquestra. Ele começa escolhendo 'k' pontos aleatórios (nossos maestros) e atribui cada membro da orquestra (ponto de dados) ao maestro mais próximo, com base em suas características. Em seguida, ele recalcula a posição do maestro com base nos membros de sua seção e repete o processo até que a orquestra esteja harmoniosamente organizada em 'k' seções.
+
+        ## Vantagens e Desvantagens:
+
+        **Vantagens**:
+        - Simplicidade e eficiência, tornando-o adequado para grandes conjuntos de dados.
+        - Resultados interpretáveis e fáceis de entender.
+
+        **Desvantagens**:
+        - O número 'k' de clusters deve ser definido antecipadamente.
+        - Sensível à inicialização, podendo cair em mínimos locais.
+        - Pode ter dificuldade com clusters de formas não esféricas.
+
+        ## Por que K-means neste projeto?
+
+        Em nossa missão de entender a pandemia, queremos segmentar a população de pacientes em grupos distintos. O K-means nos permite identificar similaridades entre os pacientes, agrupando-os de forma que cada grupo revele padrões e tendências específicas. Assim, podemos obter insights mais profundos sobre diferentes segmentos da população e adaptar estratégias de intervenção de acordo.
+        
+        """)
+
 
 with tab2: 
         pass
