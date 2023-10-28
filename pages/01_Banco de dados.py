0c10c4ea3c4e1ec13d354619b9140482740f4735
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2023-10-28 16:08:30 -0300

    Update 01_Banco de dados.py

--- pages/01_Banco de dados.py
+++ pages/01_Banco de dados.py
@@ -17,26 +17,6 @@ with tab1:
 
 
 with tab2:
-        st.markdown("""
-        # Armazenamento e Análise de Dados na AWS
-
-        Os dados do PNAD, juntamente com fontes auxiliares, foram armazenados na **Amazon S3**, onde estabelecemos a camada de dados brutos (Bronze Data). Essa camada contém dados não processados ou sem intervenção manual, todos disponíveis na nuvem.
-
-        A partir desses dados brutos, conduzimos os processos iniciais de tratamento de dados. Devido ao grande volume de dados, optamos por usar a **Amazon EC2** em conjunto com o **PySpark** para garantir um desempenho superior. Isso resultou na criação de uma nova camada de dados, agora tratados (Silver Data), que foi disponibilizada no bucket da Amazon S3.
-
-        As análises e explorações desses dados foram conduzidas usando o **Amazon Redshift**, através de consultas SQL, e a **Amazon EC2**, com a linguagem Python e o Spark. Essa etapa resultou na criação da camada final de dados, que será apresentada na aplicação Streamlit. Nessa aplicação, os resultados das análises e explorações de dados serão exibidos de forma acessível e informativa.
-
-        ## Aplicações utilizadas:
-
-        - **Amazon S3**— O Amazon Simple Storage Service (Amazon S3) é um serviço de armazenamento de objetos altamente escalável. O Amazon S3 pode ser usado para uma ampla variedade de soluções de armazenamento, incluindo sites, aplicativos móveis, backups e data lakes.
-
-        - **Amazon Redshift**— O Amazon Redshift é um serviço de armazenamento de dados totalmente gerenciado em escala de petabytes. Com o Amazon Redshift, você pode consultar petabytes de dados estruturados e semiestruturados em seu data warehouse e em seu data lake usando SQL padrão.
-
-        - **Amazon EC2**— Amazon Elastic Compute Cloud (EC2) é uma parte central da plataforma de cloud computing da Amazon.com, Amazon Web Services (AWS). O EC2 permite que os usuários aluguem computadores virtuais nos quais rodam suas próprias aplicações. O EC2 permite a implantação de aplicações escaláveis ao prover um Web service através do qual um usuário pode iniciar uma Amazon Machine Image para criar uma máquina virtual.
-        """)
-        st.image(image2)
-
-with tab3:
 
         st.markdown("""
         # Dicionário de Dados: Navegando pelas Colunas do PNAD-COVID-19
@@ -107,3 +87,22 @@ with tab3:
                     
         """)
 
+with tab3:
+        st.markdown("""
+        # Armazenamento e Análise de Dados na AWS
+
+        Os dados do PNAD, juntamente com fontes auxiliares, foram armazenados na **Amazon S3**, onde estabelecemos a camada de dados brutos (Bronze Data). Essa camada contém dados não processados ou sem intervenção manual, todos disponíveis na nuvem.
+
+        A partir desses dados brutos, conduzimos os processos iniciais de tratamento de dados. Devido ao grande volume de dados, optamos por usar a **Amazon EC2** em conjunto com o **PySpark** para garantir um desempenho superior. Isso resultou na criação de uma nova camada de dados, agora tratados (Silver Data), que foi disponibilizada no bucket da Amazon S3.
+
+        As análises e explorações desses dados foram conduzidas usando o **Amazon Redshift**, através de consultas SQL, e a **Amazon EC2**, com a linguagem Python e o Spark. Essa etapa resultou na criação da camada final de dados, que será apresentada na aplicação Streamlit. Nessa aplicação, os resultados das análises e explorações de dados serão exibidos de forma acessível e informativa.
+
+        ## Aplicações utilizadas:
+
+        - **Amazon S3**— O Amazon Simple Storage Service (Amazon S3) é um serviço de armazenamento de objetos altamente escalável. O Amazon S3 pode ser usado para uma ampla variedade de soluções de armazenamento, incluindo sites, aplicativos móveis, backups e data lakes.
+
+        - **Amazon Redshift**— O Amazon Redshift é um serviço de armazenamento de dados totalmente gerenciado em escala de petabytes. Com o Amazon Redshift, você pode consultar petabytes de dados estruturados e semiestruturados em seu data warehouse e em seu data lake usando SQL padrão.
+
+        - **Amazon EC2**— Amazon Elastic Compute Cloud (EC2) é uma parte central da plataforma de cloud computing da Amazon.com, Amazon Web Services (AWS). O EC2 permite que os usuários aluguem computadores virtuais nos quais rodam suas próprias aplicações. O EC2 permite a implantação de aplicações escaláveis ao prover um Web service através do qual um usuário pode iniciar uma Amazon Machine Image para criar uma máquina virtual.
+        """)
+        st.image(image2)
