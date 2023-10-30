25423e6db51e2f5de01764262275f25ac74f30bd
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2023-10-30 20:08:08 -0300

    up

--- pages/05_K-means.py
+++ pages/05_K-means.py
@@ -4,6 +4,7 @@ from PIL import Image
 st.set_page_config(page_title="K-means", page_icon=":house:")
 
 image = Image.open("./src/img/Model.jpg")
+imageCorrelacao = Image.open("./src/img/Dash/Kmeans/Correlacao_PNAD.png")
 st.image(image)
 
 with open("./src/css/style.css") as f:
@@ -42,7 +43,24 @@ with tab1:
 
 
 with tab2: 
-        pass
+        st.markdown("""
+        # Correlação em K-Means
+
+        A análise de correlação é uma ferramenta poderosa para identificar relações entre variáveis. No contexto do K-Means, a correlação nos ajuda a entender como diferentes características se relacionam entre si dentro dos clusters formados. Ao identificar correlações fortes, podemos obter insights valiosos sobre padrões ocultos nos dados e tomar decisões informadas.
+
+        ## Análise de Correlação
+
+        Observando o gráfico de correlação, notamos algumas relações interessantes. A contagem de sintomas apresenta uma forte relação com a ida a um estabelecimento de saúde. Isso sugere que, quanto mais sintomas um indivíduo apresenta, maior é a probabilidade de ele buscar atendimento médico. Além disso, há uma forte correlação entre os sintomas, indicando que os pacientes que apresentaram sintomas geralmente tiveram mais de um sintoma simultaneamente.
+        """)
+
+        st.image(imageCorrelacao, caption="Gráfico de Correlação dos Sintomas e Atendimento Médico.")
+
+        st.markdown("""
+        ### Conclusão
+
+        Através desta análise de correlação, conseguimos identificar padrões claros nos dados. A forte relação entre a contagem de sintomas e a busca por atendimento médico reforça a importância de monitorar e tratar os sintomas precocemente. Além disso, a correlação entre os sintomas sugere que a COVID-19 pode manifestar-se de várias maneiras em um paciente, tornando ainda mais crucial a conscientização sobre todos os possíveis sintomas da doença. À medida que avançamos, esses insights nos ajudarão a desenvolver estratégias mais eficazes para lidar com futuros surtos e melhorar o atendimento ao paciente.
+
+        """)
 
 with tab3:
         pass
