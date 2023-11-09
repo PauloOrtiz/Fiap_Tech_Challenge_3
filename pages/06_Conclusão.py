dd2c44b903015c5b44e003d1b8f75bff3a51fc07
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2023-11-08 21:34:23 -0300

    Update 06_Conclusão.py

--- pages/06_Conclusão.py
+++ pages/06_Conclusão.py
@@ -18,25 +18,14 @@ st.markdown("""
    
     ## Conclusão: Lições Aprendidas e o Caminho a Seguir:
 
-    À medida que encerramos nossa análise da PNAD 2020, emergimos com uma compreensão mais profunda do impacto da COVID-19. As histórias contadas pelos dados nos revelaram padrões e disparidades, iluminando o caminho para intervenções mais direcionadas e eficazes.
+    Em resumo, a análise abrangente da PNAD 2020 revelou insights valiosos sobre o impacto da COVID-19 no Brasil. Identificamos padrões distintos em gênero, localização geográfica, sintomas clínicos e faixa etária, fornecendo uma base sólida para a formulação de estratégias de intervenção mais direcionadas.
 
-    ### O Poder da Segmentação com K-Means
+    A aplicação de algoritmos como o K-Means trouxe uma compreensão mais refinada, destacando agrupamentos com propensões diferentes para internação. Estes grupos, oferecem insights valiosos para a priorização de recursos e estratégias.  Com este foco, estamos melhor posicionados para desenvolver políticas mais direcionadas e eficazes, principalmente para ações com o intuito de lidar com o grupo de maior risco: idosos e adultos ativos, pessoas que apresentavam mais de três sintomas, e dentre estes, sintomas respiratórios que classificamos como graves.
 
-    A aplicação do K-Means nos permitiu identificar grupos com riscos e necessidades distintas. Esta segmentação é crucial, pois nos permite focar nossos esforços nos grupos mais vulneráveis: idosos, adultos em idade ativa e aqueles com múltiplos sintomas graves.
+    Olhando para o futuro, esses resultados sugerem a necessidade contínua de abordagens interdisciplinares e adaptações metodológicas. A implementação de políticas públicas mais eficazes e equitativas requer uma compreensão mais profunda das nuances da disseminação do vírus.
 
-    ### Estratégias para um Futuro Incerto
+    Em meio aos desafios, a riqueza de dados da PNAD 2020 abre portas para investigações mais aprofundadas e aprimoramento das estratégias existentes. Ao avançarmos, a combinação de tecnologia, análise de dados e colaboração interdisciplinar certamente impulsionará nossa capacidade de enfrentar futuras crises de saúde com resiliência e eficácia. 
 
-    Os insights obtidos não são apenas um reflexo do passado, mas também uma bússola para o futuro. Eles nos equipam para desenvolver políticas de saúde mais assertivas, especialmente em preparação para possíveis novos surtos.
-
-    ### A Sinergia entre Dados e Ação
-
-    Os dados da PNAD 2020 são um convite à ação. Eles nos desafiam a transformar análises em estratégias concretas, a utilizar a tecnologia como uma aliada e a promover a colaboração interdisciplinar como nunca antes.
-
-    ### Avançando com Resiliência
-
-    À medida que avançamos, levamos conosco as lições aprendidas e a determinação de aplicar esse conhecimento em prol de uma sociedade mais resiliente. Que as descobertas da PNAD 2020 inspirem e orientem o Brasil rumo a um futuro onde a saúde e o bem-estar são priorizados.
-
-            
     ## Ações Recomendadas:
 
     Com base em nosso entendimento, aqui estão algumas ações que o hospital pode considerar caso enfrentemos um novo surto da doença:
