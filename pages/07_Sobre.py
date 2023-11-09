0a631b127dccea0f5b0436cb3185f01f4a3dddf5
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2023-11-08 21:21:56 -0300

    Update 07_Sobre.py

--- pages/07_Sobre.py
+++ pages/07_Sobre.py
@@ -11,35 +11,36 @@ with open("./src/css/style.css") as f:
 
 
 st.markdown("""
-    # Sobre o Projeto
-    
-    Este projeto foi desenvolvido como parte do 3º Tech Challenge da turma de Data Science da FIAP. Foi uma oportunidade incrível para aprimorar nossas habilidades em análise de dados e colaboração em equipe, e gostaríamos de expressar nossa gratidão a todos que participaram deste desafio.
+        # Sobre o Projeto
 
-    ## Equipe do Projeto 
-    
-    - Amanda Bueno de Oliveira
-    - João Guilherme Simões
-    - Luiz Antonio Simette de Mello Campos
-    - Paulo Henrique Barbosa Ortiz de Souza
-            
+        Este projeto foi desenvolvido como parte do 3º Tech Challenge da turma de Data Science da FIAP. Foi uma oportunidade incrível para aprimorar nossas habilidades em análise de dados e colaboração em equipe, e gostaríamos de expressar nossa gratidão a todos que participaram deste desafio.
 
-    ## Orientadores 
-    
-    Este projeto não teria sido possível sem a orientação de nossos professores:
+        ## Equipe do Projeto 
 
-    - Edgard Joseph Kiriyama
-    - Matheus Pavani
+        - Amanda Bueno de Oliveira
+        - João Guilherme Simões
+        - Luiz Antonio Simette de Mello Campos
+        - Paulo Henrique Barbosa Ortiz de Souza
+                
 
-    Eles nos forneceram a estrutura necessária para concluir este desafio e estamos profundamente gratos pelo tempo e esforço que dedicaram à nossa aprendizagem.
+        ## Orientadores 
 
-    ##  Agradecimentos
+        Este projeto não teria sido possível sem a orientação de nossos professores:
 
-    Também gostaríamos de agradecer aos nossos colegas de classe e a todos os envolvidos na organização deste desafio.
-        
-    ## Referências 📚
+        - Edgard Joseph Kiriyama
+        - Matheus Pavani
 
-    **Guangyang Li. Documentação do pywaffle**. Disponível em: https://pywaffle.readthedocs.io/en/latest/index.html. Acesso em: 12 de Outubro de 2023.
+        Eles nos forneceram a estrutura necessária para concluir este desafio e estamos profundamente gratos pelo tempo e esforço que dedicaram à nossa aprendizagem.
 
-        
-    Gostaríamos de expressar nossa gratidão a todas estas fontes por disponibilizar esses dados publicamente.
-    """, unsafe_allow_html=True)
+        ##  Agradecimentos
+
+        Também gostaríamos de agradecer aos nossos colegas de classe e a todos os envolvidos na organização deste desafio.
+
+        ## Referências 📚
+
+        **Guangyang Li. Documentação do pywaffle**. Disponível em: https://pywaffle.readthedocs.io/en/latest/index.html. Acesso em: 12 de Outubro de 2023.
+                
+        **DAITYARL, Shaumik . Cluster Analysis in Python**. DataCamp, 2023. Disponível em: https://app.datacamp.com/learn/courses/cluster-analysis-in-python. Acesso em: 23 de outubro de 2023.
+
+        Gostaríamos de expressar nossa gratidão a todas estas fontes por disponibilizar esses dados publicamente.
+        """, unsafe_allow_html=True)
